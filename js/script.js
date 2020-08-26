772e88f75082767f3dae58ead1fa8dea779dd6cc
Jordan <jordandevaney28@gmail.com>
2020-08-26 18:04:57 -0400

    fixed bug that prenventing number cells that are alive from increment. Display generation on next generation of cell but counter keeps going after the cell stop animating. Bugs, Bugs, Bugs, everywhere there is bugs

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@ let container = document.querySelector('.main')
 let canvas = document.querySelector('.grid')
 let ctx = canvas.getContext('2d')
 
-let rows = 16
 let startBtn = document.querySelector('.start')
 let endBtn = document.querySelector('.end')
 let column = document.getElementsByClassName('column')
 let clearBtn = document.querySelector('.clear')
-let cols = 16
-let universe;
+let generation = 0
+let generation_tag = document.querySelector('.generation')
 
 let resolution = 40
 canvas.height = 800
 canvas.width = 800
+
+let rows = 16
+let cols = 16
+let timeOutNum = 500
 
 function buildGrid() {
     return new Array(cols).fill(null)
@@ -21,24 +24,30 @@ function buildGrid() {
 }
 
 let grid = buildGrid()
-// setInterval(() => {
-    update()
-// }, 1000)
-//     startBtn.addEventListener('click', (e) => {
-//         // make cell unclicked when you hit start button
-//         for (let i = 0; i < column.length; i++) {
-//             column[i].classList.add('no_click')
-//         }
-//     })
-// }
+renderGrid(grid)
+
+    // start animating on click
+startBtn.addEventListener('click', (e) => {
+    setTimeout(() => {
+        update()
+    }, timeOutNum)
+    // make cell unclicked when you hit start button
+    for (let i = 0; i < column.length; i++) {
+        column[i].classList.add('no_click')
+    }
+})
 
 function update() {
-    // the bug is in next gen grid
     grid = nextGenGrid(grid)
     renderGrid(grid)
-    // setInterval(() => {
-    //     update()
-    // }, 1000)
+
+    // TODO: generation incrementer doesn't stop when the animation does
+    generation += 1
+    generation_tag.innerText = `generation ${generation}`
+
+    setTimeout(() => {
+        update()
+    }, timeOutNum)
 }
 
 function renderGrid(grid) {
@@ -54,21 +63,9 @@ function renderGrid(grid) {
         }
     }
 }
-
-function makeNew2dArray(cols, row, grid) {
-
-    let newArray = new Array(cols)
-    for (let i = 0; i < grid.length; i++) {
-        newArray[cols] = new Array(row)
-    }
-    return newArray
-}
 // this function make a grid for the next generation
 function nextGenGrid(grid) {
-    console.table(grid)
-
     let nextGen = grid.map(array => [...array])
-    console.table(nextGen)
     for (let col = 0; col < grid.length; col++) {
         for (let row = 0; row < grid[col].length; row++) {
             let cell = grid[col][row]
@@ -84,14 +81,9 @@ function nextGenGrid(grid) {
                     const y_cell = row + j
 
                     // checking if we it checks outside of the bounds of the grid 
-                    // TODO: not counting neihbors correctly
-                    if( x_cell >= 0 && y_cell >=0 && x_cell < col && y_cell < row) {
-                        // say that neighbor is dead
-                        console.log(`\nchecking cell ${cell}`)
-                        let currentNeighbor = grid[x_cell][y_cell]
-                        console.log(`ncurrent neighbor is`, currentNeighbor)
+                    if( x_cell >= 0 && y_cell >=0 && x_cell < cols && y_cell < rows) {
+                        const currentNeighbor = grid[col + i][row + j]
                         numNeighbors += currentNeighbor
-                        console.log(`numNeighbors is ${numNeighbors}\n`)
                     } 
                 }
             }
@@ -99,30 +91,18 @@ function nextGenGrid(grid) {
             // 
             // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
             if(cell === 1 & numNeighbors < 2) {
-                // console.log(`cell at ${col}:${row} has less than 2 neighbors on or more than 3 neighbors on`)
                 nextGen[col][row] = 0
-                // Any live cell with two or three live neighbours lives on to the next generation.
-            } else if (cell=== 1 && numNeighbors === 2 || numNeighbors === 3) {
-                // console.log(`cell at ${col}:${row} has less than 2 neighbors on or more than 3 neighbors on`)
-                nextGen[col][row] = 1
-                
+            } 
                 // Any live cell with more than three live neighbours dies, as if by overpopulation.
-                // TODO: this is where our bug lies
-            } else if (cell === 1 && numNeighbors > 3) {
-                console.log(`cell at ${col}:${row} has less than 2 neighbors on or more than 3 neighbors on`)
-                nextGen[col][row] = 1
-                // console.log(nextGen)
+            else if (cell === 1 && numNeighbors > 3) {
+                nextGen[col][row] = 0
             }
             // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
             else if (cell === 0 && numNeighbors === 3) {
-                console.log(`cell at ${col}:${row} has less than 2 neighbors on or more than 3 neighbors on`)
                 nextGen[col][row] = 1
-                // console.log(nextGen)
             }
-        
         }
     }
-    console.table('next gen grid after modification',nextGen)
     return nextGen
 }
 
