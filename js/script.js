4487ac2b7d8d194f650fee76f4877a8f55d45bd3
Jordan <jordandevaney28@gmail.com>
2020-08-26 00:09:11 -0400

    use canvas to draw grid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let container = document.querySelector('.main')
-let grid = document.querySelector('.grid')
+let canvas = document.querySelector('.grid')
+let ctx = canvas.getContext('2d')
 
 let rows = 16
 let startBtn = document.querySelector('.start')
@@ -7,159 +8,138 @@ let endBtn = document.querySelector('.end')
 let column = document.getElementsByClassName('column')
 let clearBtn = document.querySelector('.clear')
 let cols = 16
+let universe;
 
-//make grid
+let resolution = 40
+canvas.height = 800
+canvas.width = 800
 
-
-function make2DArray(cols, rows) {
-    let arr = new Array(cols)
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows)
-    }
-    return arr
-}
-function randomGrid() {
-    let universe = make2DArray(cols, rows)
-    // store what cells are dead into array
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            // 0 signifies a dead cell
-            // 1 signifies an alive cell
-            universe[i][j] = Math.floor(Math.random() * 2)
-        }
-    } 
-    return universe
+function buildGrid() {
+    return new Array(cols).fill(null)
+        .map(() => new Array(rows).fill(null)
+        .map(() => Math.floor(Math.random() * 2)))
 }
 
-// generate the UI Grid
-function generateUIGrid(universe) {
-    let column = ''
-    for (let i = 0; i < universe.length; i++) {
-        let row = document.createElement('div')
-        row.className = 'row'
-        for (let j = 0; j < universe[i].length; j++) {
-            column = document.createElement("div");
-            column.className = `col-xs cell${i}:${j}`
-            if (universe[i][j] === 1) {
-                column.style.backgroundColor = 'black'
-            } else {
-                column.style.backgroundColor = 'white'
-            }
-            // configureColumn(column)
-            column.innerText = ' '
-            row.appendChild(column)
-        }
-        grid.appendChild(row)
-    }
-    return grid
-    // TODO: how do I replace this grid with the new generation
-};
+let grid = buildGrid()
+// requestAnimationFrame(update)
 
-
-// Why do I get an infinite loop
-function nextGenGrid(arr) {
-    // this function make a grid for the next generation
-    console.log(`the arr in nextGenGrid is ${arr}`)
-    let operations = [
-        [0, 1],
-        [0, -1],
-        [0, +1],
-        [1, -1],
-        [1, +1],
-        [+1, -1],
-        [+1, 1],
-        [+1, +1]
-    ]
-    // row 
-    for (let i = 0; i < arr.length; i++) {
-        // column
-        for (let j = 0; j < arr[i].length; j++) {
-            let neighbors = 0
-            // console.log(`cell at ${i}:${j} is, ${arr[i][j]}`)
-            operations.forEach(([x, y]) => {
-                let newI = x + j;
-                let newJ = y + j
-                // check the bounds
-                if(newI >= 0  && newI < arr.length && newJ >= 0 && newJ < arr[i].length) {
-                    neighbors += arr[newI][newJ]
-                    // console.log(`neighbor value for ${i}:${j} are ${neighbors}`)
-                }
-                if(neighbors < 2 || neighbors > 3) {
-                    // console.log(`cell at ${i}:${j} has less than 2 neighbors on or more than 3 neighbors on`)
-                    arr[i][j] = 0
-                } else if (arr[i][j] === 0 && neighbors === 3) {
-                    arr[i][j] = 1
-                }
-            })
-        }
-        // make UI updates
-        generateUIGrid(arr)
-    }
-}
-window.onload = () => {
-    let randomArr = randomGrid()
-    generateUIGrid(randomArr)
-
-    startBtn.addEventListener('click', (e) => {
-        // run computer geration grid
-        // TODO: conect the update grid to the UI grid
-        setTimeout(nextGenGrid(randomArr), 1000)
-        // make cell unclicked when you hit start button
-        for (let i = 0; i < column.length; i++) {
-            column[i].classList.add('no_click')
-        }
-    })
-}
-
-    // // endBtn.addEventListener('click', (e) => {
-        
-    //     // })
-    //     // clear classes on clear button click
-    //     clearBtn.addEventListener('click', (e) => {
-    //         for (let i = 0; i < column.length; i++) {
-    //             column[i].classList.remove('alive')
-    //             column[i].classList.remove('no_click')
-    //         }
-    //     })
-    // }
-    
-    // // user clicking the column
-    // const configureColumn = (column) => {
-    //     column.addEventListener('click', (e) => {
-    //         // if you click on the cell its alive
-    //         // TODO: toggling would be better but for some reason I could not make that work
-    //         if( e.target.classList.contains('dead')) {
-    //             e.target.classList.remove('dead')
-    //             e.target.classList.add('alive')
-    //         } else {
-    //             e.target.classList.remove('alive')
-    //             e.target.classList.add('dead')
-    //         }
-    //     })
-    // }
-    // Behaviors
-    
-    // # Toggle state functionality: switch between alive & dead either because user manually toggled cell before starting simulation or simulation is running and rules of life caused cell to change state
-    
-    // # Utilize a timeout function to build the next generation of cells & update the display at the chosen time interval
-    
-    // function changeGeneration() {
-//     setTimeout(function () {
-//         // display new generation grid after cells change between dead and alive
-//         // increment geration counter by one
-//     },3000)
+// function update() {
+    grid = nextGenGrid(grid)
+    renderGrid(grid)
+    //     requestAnimationFrame(update)
 // }
 
-// # Write an algorithm that:
+function renderGrid(grid) {
+    for (let col = 0; col < grid.length; col++) {
+        for (let row = 0; row < grid[col].length; row++) {
+            let cell = grid[col][row]
 
-// # Implements the following basic steps:
-// # For each cell in the current generation's grid:
-// # Examine state of all eight neighbors (it's up to you whether you want cells to wrap around the grid and consider cells on the other side or not)
-// # Apply rules of life to determine if this cell will change states
-// # When main loop completes:
-// # Swap current and next grids
-// # Repeat until simulation stopped
-// # Breaks down above steps into appropriate sub-tasks implemented with helper functions to improve readability
-// # Uses double buffering to update grid with next generation.
-// # Does something well-documented with the edge of the grid. (e.g. wrap around to the far side--most fun!--or assumes all edge cells are permanently dead.)
+            ctx.beginPath()
+            ctx.rect(col * resolution, row * resolution, resolution, resolution)
+            ctx.fillStyle = cell ? 'black': 'white'
+            ctx.fill()
+            ctx.stroke()
+        }
+    }
+}
 
+// function makeNew2dArray() {
+
+//     let newArray = new Array(grid.length)
+//     for (let col = 0; col < grid.length; col++) {
+
+//         newArray[col] = new Array()
+//         for (let row = 0; row < grid[col].length; row++) {
+//         }
+//     }
+// }
+function nextGenGrid(grid) {
+    // this function make a grid for the next generation
+    console.log('initial array', grid)
+    // let nextGen = new Array(grid.length)
+    let nextGen = Array.from(grid, () => new Array(grid.length))
+    console.log(nextGen)
+    for (let col = 0; col < grid.length; col++) {
+        for (let row = 0; row < grid[col].length; row++) {
+            let cell = grid[col][row]
+            let numNeighbors = 0
+            // looping through neighbors of the current cell
+            for (let i = -1; i < 2; i++) {
+                for (let j = -1; j < 2; j++) {
+                    // don't count the cell we are currently on
+                    if( i === 0 && j === 0) {
+                        continue
+                    }
+                    const x_cell = col + i
+                    const y_cell = row + j
+
+                    if( x_cell >= 0 && y_cell >=0 && x_cell < col && y_cell < row) {
+                        const currentNeighbor = grid[col + i][row + j]
+                        numNeighbors += currentNeighbor
+                        // console.log(numNeighbors)
+                    }
+                }
+            }
+            if(cell === 1 & numNeighbors < 2) {
+                // console.log(`cell at ${i}:${j} has less than 2 neighbors on or more than 3 neighbors on`)
+                console.log(cell)
+                nextGen[col][row] = 0
+            } else if (cell=== 1 && numNeighbors > 3) {
+                console.log(cell)
+                nextGen[col][row] = 0
+            } else if (cell === 0 && numNeighbors === 3) {
+                nextGen[col][row] = 1
+                console.log(cell)
+            }
+        }
+    }
+    return nextGen
+}
+// }
+// window.onload = () => {
+//     randomGrid()
+//     generateUIGrid(universe)
+
+
+//     startBtn.addEventListener('click', (e) => {
+//         // run computer geration grid
+//         // TODO: conect the update grid to the UI grid
+//         let updatedArray = nextGenGrid()
+//         console.log('new array will be', updatedArray)
+//         // make UI updates
+//         setTimeout(() => {
+//             generateUIGrid(updatedArray)
+//         }, 1000)
+//         // make cell unclicked when you hit start button
+//         for (let i = 0; i < column.length; i++) {
+//             column[i].classList.add('no_click')
+//         }
+//     })
+// }
+
+//     // // endBtn.addEventListener('click', (e) => {
+        
+//     //     // })
+//     //     // clear classes on clear button click
+//     //     clearBtn.addEventListener('click', (e) => {
+//     //         for (let i = 0; i < column.length; i++) {
+//     //             column[i].classList.remove('alive')
+//     //             column[i].classList.remove('no_click')
+//     //         }
+//     //     })
+//     // }
+    
+//     // // user clicking the column
+//     // const configureColumn = (column) => {
+//     //     column.addEventListener('click', (e) => {
+//     //         // if you click on the cell its alive
+//     //         // TODO: toggling would be better but for some reason I could not make that work
+//     //         if( e.target.classList.contains('dead')) {
+//     //             e.target.classList.remove('dead')
+//     //             e.target.classList.add('alive')
+//     //         } else {
+//     //             e.target.classList.remove('alive')
+//     //             e.target.classList.add('dead')
+//     //         }
+//     //     })
+//     // }
